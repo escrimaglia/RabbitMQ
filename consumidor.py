07368302ef9fefb37e2d6d7ed5e4f33fcb4b0b70
Ed Scrimaglia <edgardo.scrimaglia@gmail.com>
2024-11-02 20:59:52 -0300

    refactoring message error consumer

--- consumidor.py
+++ consumidor.py
@@ -3,6 +3,8 @@
 
 import pika
 
+used_queue = 'cola_test'
+
 def callback(ch, method, properties, body):
     print(f" [x] Received {body.decode()}")
 
@@ -11,15 +13,20 @@ def consume_messages():
         connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
         channel = connection.channel()
 
-        channel.queue_declare(queue='cola_test')
+        channel.queue_declare(queue=used_queue)
 
-        channel.basic_consume(queue='cola_test', on_message_callback=callback, auto_ack=True)
+        channel.basic_consume(queue=used_queue, on_message_callback=callback, auto_ack=True)
 
         print(' [*] Leyendo cola en RabbitMQ. To exit press CTRL+C')
         
         channel.start_consuming()
+    except pika.exceptions.AMQPConnectionError as err:
+        print ("RabbitMQ connection error")
+    except pika.exceptions.AMQPChannelError as err:
+        print (f"RabbitMQ queue {used_queue} can not be used")
     except (Exception, KeyboardInterrupt) as err:
         print (err)
 
 if __name__ == "__main__":
     consume_messages()
+   
